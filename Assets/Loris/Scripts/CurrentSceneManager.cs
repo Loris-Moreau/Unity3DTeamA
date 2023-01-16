478b799cbc08e8871a9dac697a638355f1916846
Loris M <80474693+Loris-Moreau@users.noreply.github.com>
2023-01-16 16:29:47 +0100

    Loris scene testing

---
+++ Assets/Loris/Scripts/CurrentSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CurrentSceneManager : MonoBehaviour
+{
+    public int coinsPickedUpInThisSceneCount;
+    public Vector3 respawnPoint;
+    public int levelToUnlock;
+
+    public static CurrentSceneManager instance;
+    
+    private int MobCount = 0;
+    private int MobLimit = 25;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de CurrentSceneManager dans la scène");
+            return;
+        }
+
+        instance = this;
+
+        respawnPoint = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    public void SetMobCount(int amount)
+    {
+        MobCount += amount;
+    }
+
+    public int GetMobCount()
+    {
+        return MobCount;
+    }
+
+    public int GetMobLimit()
+    {
+        return MobLimit;
+    }
+}
---
+++ Assets/Loris/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string nameItem;
+    public string description;
+    public Sprite image;
+    public int hpGiven;
+    public int speedGiven;
+    public float speedDuration;
+    public int damageGiven;
+    public GameObject itemPrefab;
+}
