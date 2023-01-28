4c9703aab5ae5c00cbc91930accfdc032006ce5f
Wolf Rex <80474693+Loris-Moreau@users.noreply.github.com>
2023-01-28 15:17:38 +0100

    mouse look (it almost works)

--- Assets/Loris/Scripts/MouseLook.cs
+++ Assets/Loris/Scripts/MouseLook.cs
@@ -1,17 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;
-    public Transform Gun;
+    public float sensitivity = 100.0f;
 
-    private float xRotationCamera = 0f;
-    private float xRotationGun = 90f;
+    private Vector2 mouseInput;
 
     void Start()
     {
@@ -20,26 +15,10 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        
-    }
+        // Get mouse input
+        mouseInput = InputSystem.GetDevice<Mouse>().delta.ReadValue();
 
-    public void MouseMouvement(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-            xRotationCamera -= mouseY;
-            xRotationCamera = Mathf.Clamp(xRotationCamera, -90f, 90f);
-
-            xRotationGun -= mouseY;
-            xRotationGun = Mathf.Clamp(xRotationGun, 0f, 180f);
-            
-            transform.localRotation = Quaternion.Euler(xRotationCamera, 0f, 0f);
-
-            playerBody.Rotate(Vector3.up * mouseX);
-            Gun.localRotation = Quaternion.Euler(xRotationGun, 0f, 0f);
-        }
+        // Rotate camera based on mouse input
+        transform.Rotate(-mouseInput.y * sensitivity * Time.deltaTime, mouseInput.x * sensitivity * Time.deltaTime, 0);
     }
 }
--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -5,6 +5,12 @@ using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
+    [Header("Mouse Follow")]
+    public InputAction mousePosition;
+    public float rotationSpeed = 100.0f;
+    public float rotationSmoothSpeed = 0.2f;
+    private Quaternion targetRotation;
+
     [Header("Movements")]
     public float speed = 5;
     private Vector2 direction;
@@ -24,11 +30,32 @@ public class Controller : MonoBehaviour
     private void Start()
     {
         transform.position = respawnPoint.position;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
         transform.position += speed * Time.deltaTime * new Vector3(direction.x, 0, direction.y);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothSpeed * Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnEnable()
+    {
+        mousePosition.performed += UpdateRotation;
+    }
+
+    private void OnDisable()
+    {
+        mousePosition.performed -= UpdateRotation;
+    }
+
+    private void UpdateRotation(InputAction.CallbackContext context)
+    {
+        Vector2 mouseDelta = context.ReadValue<Vector2>();
+        float angle = Mathf.Atan2(mouseDelta.y, mouseDelta.x) * Mathf.Rad2Deg;
+        targetRotation = Quaternion.Euler(0, angle, 0);
     }
 
     public void Move(InputAction.CallbackContext context)
