387a2192bca280a51d9fcd088feae27643a6791a
L3o <74620207+leorchr@users.noreply.github.com>
2023-02-10 09:20:25 +0100

    Merge fix

--- Assets/Script/ControllerBis.cs
+++ Assets/Script/ControllerBis.cs
@@ -1,9 +1,10 @@
+using TMPro;
 using UnityEditor;
 using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Controller : MonoBehaviour
+public class ControllerBis : MonoBehaviour
 {
     #region Movement
     [Space]
@@ -31,6 +32,7 @@ public class Controller : MonoBehaviour
     [Space]
 
     public GameObject interactMessage;
+    private TextMeshProUGUI interactionMsg;
     #endregion
 
     #region Animations
@@ -49,7 +51,6 @@ public class Controller : MonoBehaviour
 
     public InputAction mousePosition;
     public float rotationSpeed = 10.0f;
-
     private Quaternion targetRotation;
     private Quaternion currentRotation;
     public float rotationThreshold = 0.1f;
@@ -60,8 +61,7 @@ public class Controller : MonoBehaviour
     {
         transform.position = respawnPoint.position;
 
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
@@ -70,22 +70,33 @@ public class Controller : MonoBehaviour
 
         currentRotation = transform.rotation;
         transform.rotation = Quaternion.RotateTowards(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        Debug.Log("Current");
+        Debug.Log(currentRotation);
+        Debug.Log("Taget");
+        Debug.Log(targetRotation);
     }
 
-    public void UpdateRotation(InputAction.CallbackContext context)
+    private void OnEnable()
+    {
+        mousePosition.performed += UpdateRotation;
+    }
+
+    private void OnDisable()
+    {
+        mousePosition.performed -= UpdateRotation;
+    }
+
+    private void UpdateRotation(InputAction.CallbackContext context)
     {
         Vector2 mouseDelta = context.ReadValue<Vector2>();
-        Debug.Log(mouseDelta);
-        if (Mathf.Abs(mouseDelta.x) !=0)
-        {
-            transform.Rotate(0, 0.5f * mouseDelta.x, 0);
-        }
+        float angle = -Mathf.Atan2(mouseDelta.y, mouseDelta.x) * Mathf.Rad2Deg;
+        targetRotation = Quaternion.Euler(0, angle, 0);
 
-        if (Mathf.Abs(mouseDelta.y) !=0)
+        if(Quaternion.Angle(currentRotation, targetRotation) < rotationThreshold)
         {
-            transform.Rotate(0.5f * mouseDelta.y, 0, 0);
+            currentRotation = targetRotation;
         }
-
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -108,8 +119,15 @@ public class Controller : MonoBehaviour
         if(collision.gameObject.tag == "Bed")
         {
             interactMessage.SetActive(true);
+            interactionMsg.text = "Une bonne nuit de sommeil s'impose...";
             isBed = true;
             currentBed = collision.transform;
+        }
+        else if(collision.gameObject.tag == "Medikit")
+        {
+            interactMessage.SetActive(true);
+            interactionMsg.text = "Heal (1)";
+
         }
     }
 
@@ -120,5 +138,11 @@ public class Controller : MonoBehaviour
             interactMessage.SetActive(false);
             isBed = false;
         }
+        else if(other.gameObject.tag == "Medikit")
+        {
+            interactMessage.SetActive(false);
+        }
     }
+
+    
 }
