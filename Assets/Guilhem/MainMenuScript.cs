4309bd663a7052daac46aa32bedd4ce95ea2c12d
Komediem <gbussola@artfx.fr>
2023-02-24 00:57:03 +0100

    MainMenu Animations

--- Assets/Guilhem/MainMenuScript.cs
+++ Assets/Guilhem/MainMenuScript.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator CameraAnimator;
+    public GameObject Buttons;
+    public GameObject settingsMenu;
+
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Level");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SettingsMenu()
     {
-        
+        StartCoroutine(SettingsMenuPressed());
+    }
+
+    public void Back()
+    {
+        StartCoroutine(SettingsMenuDesactivation());
+    }
+
+    IEnumerator SettingsMenuPressed()
+    {
+        Buttons.SetActive(false);
+        CameraAnimator.SetBool("IsPressed", true);
+        yield return new WaitForSeconds(1.75f);
+        settingsMenu.SetActive(true);
+    }
+
+    IEnumerator SettingsMenuDesactivation()
+    {
+        settingsMenu.SetActive(false);
+        CameraAnimator.SetBool("IsPressed", false);
+        yield return new WaitForSeconds(1.75f);
+        Buttons.SetActive(true);
     }
 }
